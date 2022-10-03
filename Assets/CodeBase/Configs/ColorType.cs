ef6af6819be7e0413952624d766706b5b48c7c79
RomanDemido <roman199798@gmail.com>
2022-10-03 09:25:14 +0300

    Add configs

--- Assets/CodeBase/Configs/ColorType.cs
+++ Assets/CodeBase/Configs/ColorType.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace codeBase.configs
+﻿namespace codeBase.configs
 {
     public enum ColorType
     {
         BLUE,
         GREEN,
         RED,
-        YELLOW,
+        YELLOW
     }
 }
--- Assets/CodeBase/Configs/Constants.cs
+++ Assets/CodeBase/Configs/Constants.cs
@@ -2,8 +2,12 @@
 {
     public static class Constants
     {
+        public const float DELTATIME = 0.02f;
         public const float MAX_RAY_DISTANCE = 1000f;
         public const float MIN_MOVEMENT_DISTANCE = 0.05f;
         public const string ANIMATION_WALKING = "IsWalking";
+        public const string ECS_SISTEM_UPDATE_ID = "updateSystem";
+        public const string ECS_SISTEM_FIXED_UPDATE_ID = "fixedUpdateSystem";
+
     }
 }
